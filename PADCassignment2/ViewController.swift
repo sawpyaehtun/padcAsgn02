6f428689c63f29fd5f371e2dda2a3a1947cf23e3
sawpyae <sawpyae.htun@haulio.io>
2019-08-08 18:18:48 +0630

    Initial Commit

---
+++ PADCassignment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PADCassignment2
+//
+//  Created by saw pyaehtun on 08/08/2019.
+//  Copyright © 2019 saw pyaehtun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
